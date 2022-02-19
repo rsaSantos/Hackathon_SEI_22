ec025161a74f789b7ac460aaa7256d8caf40ffd4
rsaSantos <rs.elsaint@gmail.com>
2022-02-19 15:03:06 +0000

    script_js updated -> somos uns patos

---
+++ Requests_scripts/new_script.js
@@ -0,0 +1,30 @@
+// LIST OF REQUESTS TO BE MADE: http://localhost:8080/
+//
+// Ingrediente = {"nome":"Cebola","quantidade":1,"sistemaNumerico":"un"}
+//
+// nEmentas?numEmentas=6
+// Escolhe n ementas do sistema (devolve [nomeEmenta, link foto], [Ingredientes] aka lista de compras): 
+//
+// todasEmentas
+// Devolve todas as ementas: [nomeEmenta, link foto]
+//
+// receita?nomeEmenta=bacalhau+com+natas
+// Devolve ementa e seus ingredientes: nomeEmenta, link foto, [Ingredientes]
+//
+
+import axios from 'redaxios';
+
+
+const API = axios.create({
+    baseURL: "http://localhost:8080/",
+    responseType: 'json',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export default API;
+
+// João - frontend
+// const dados = await API.get("/nEmentas")
+//if dados.statusCode > 400 
